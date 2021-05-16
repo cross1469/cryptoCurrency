a6ac0caf35962e540dc0a49e6842fdd888094842
cross1469 <face9923029@gmail.com>
2021-05-16 17:59:04 +0800

    Complete Search coin data feature

--- src/component/CoinData.js
+++ src/component/CoinData.js
@@ -3,6 +3,12 @@ import { Table, Thead, Tr, Th, Tbody, Td } from "@bootstrap-styled/v4";
 
 const CoinData = () => {
   const [realTimeDatas, setRealTimeDatas] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
   const realTimeCoinData = () => {
     const socket = new WebSocket(
@@ -24,28 +30,59 @@ const CoinData = () => {
     realTimeCoinData();
   }, []);
 
+  useEffect(() => {
+    if (JSON.stringify(realTimeDatas) !== "[]") {
+      const results = realTimeDatas.filter((realTimeData) =>
+        realTimeData.s.includes(searchTerm)
+      );
+      setSearchResults(results);
+    }
+  }, [searchTerm]);
+
   if (JSON.stringify(realTimeDatas) === "[]") {
     return null;
   }
 
-  const renderCoinData = () =>
-    realTimeDatas.map((realTimeData) => (
-      <Tr key={realTimeData.L + 1}>
-        <Td scope="row">{realTimeData.s}</Td>
-        <Td>{realTimeData.c}</Td>
-        <Td>{realTimeData.P}</Td>
-        <Td>{realTimeData.h}</Td>
-        <Td>{realTimeData.l}</Td>
-        <Td>{realTimeData.v}</Td>
+  const renderCoinDatas = () => {
+    if (searchTerm === "") {
+      return realTimeDatas.map((realTimeData) => (
+        <Tr key={realTimeData.L + 1}>
+          <Td scope="row">{realTimeData.s}</Td>
+          <Td>{realTimeData.c}</Td>
+          <Td>{realTimeData.P}</Td>
+          <Td>{realTimeData.h}</Td>
+          <Td>{realTimeData.l}</Td>
+          <Td>{realTimeData.v}</Td>
+          <Td>
+            <button type="button">收藏</button>
+          </Td>
+        </Tr>
+      ));
+    }
+    return searchResults.map((item) => (
+      <Tr key={item.L + 1}>
+        <Td scope="row">{item.s}</Td>
+        <Td>{item.c}</Td>
+        <Td>{item.P}</Td>
+        <Td>{item.h}</Td>
+        <Td>{item.l}</Td>
+        <Td>{item.v}</Td>
         <Td>
           <button type="button">收藏</button>
         </Td>
       </Tr>
     ));
+  };
 
   return (
     <>
       <h2>貨幣資料</h2>
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={handleChange}
+      />
       <Table id="CoinDatas" hover>
         <Thead>
           <Tr color="active">
@@ -58,7 +95,7 @@ const CoinData = () => {
             <Th>收藏</Th>
           </Tr>
         </Thead>
-        <Tbody>{renderCoinData()}</Tbody>
+        <Tbody>{renderCoinDatas()}</Tbody>
       </Table>
     </>
   );
