183f727f6b018034d35f3ff05b2f3f055257d29d
cross1469 <face9923029@gmail.com>
2021-05-18 09:55:19 +0800

    Complete initial header layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from "react";
-import GridLayout from "react-grid-layout";
-import KLine from "./Component/KLine";
-import AreaSpline from "./Component/AreaSpline";
-import PlaceOrder from "./Component/PlaceOrder";
-import AssetsForm from "./Component/AssetsForm";
-import Chat from "./Component/Chat";
-import WishList from "./Component/WishList";
-import CoinData from "./Component/CoinData";
-import Sign from "./Component/Sign";
+// import GridLayout from "react-grid-layout";
+import { Reset } from "styled-reset";
+import Header from "./Component/Header";
+
+// import KLine from "./Component/KLine";
+// import AreaSpline from "./Component/AreaSpline";
+// import PlaceOrder from "./Component/PlaceOrder";
+// import AssetsForm from "./Component/AssetsForm";
+// import Chat from "./Component/Chat";
+// import WishList from "./Component/WishList";
+// import CoinData from "./Component/CoinData";
+// import Sign from "./Component/Sign";
 
 import "./Utils/firebase";
 
 function App() {
-  const layout = [
-    { i: "a", x: 0, y: 0, w: 12, h: 3, static: true },
-    { i: "b", x: 5, y: 0, w: 12, h: 2.5, minW: 2, maxW: 12 },
-    { i: "c", x: 10, y: 0, w: 12, h: 1 },
-  ];
+  // const layout = [
+  //   { i: "a", x: 0, y: 0, w: 12, h: 3, static: true },
+  //   { i: "b", x: 5, y: 0, w: 12, h: 2.5, minW: 2, maxW: 12 },
+  //   { i: "c", x: 10, y: 0, w: 12, h: 1 },
+  // ];
   return (
     <div className="App">
-      <GridLayout className="layout" layout={layout} cols={12} width={1200}>
+      <Reset />
+      <Header />
+      {/* <GridLayout className="layout" layout={layout} cols={12} width={1200}>
         <div key="a">
           <KLine />
         </div>
@@ -34,7 +39,7 @@ function App() {
       <Chat />
       <WishList />
       <Sign />
-      <CoinData />
+      <CoinData /> */}
     </div>
   );
 }
---
+++ src/component/Header.js
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+
+const HeaderStyled = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  outline: none;
+  width: 100%;
+  background: #000;
+  padding-bottom: 8px;
+`;
+
+const Logo = styled.a`
+  text-decoration: none;
+  margin-left: 24px;
+  cursor: pointer;
+  color: #fff;
+  padding-top: 16px;
+  padding-bottom: 16px;
+`;
+
+const Nav = styled.ul`
+  display: flex;
+`;
+
+const NavItem = styled.li`
+  margin-right: 16px;
+`;
+
+const NavItemLink = styled.a`
+  padding-top: 16px;
+  padding-bottom: 16px;
+  text-decoration: none;
+  cursor: pointer;
+  color: #fff;
+  :hover {
+    border-bottom: 2px solid orange;
+  }
+`;
+
+const Header = () => (
+  <HeaderStyled>
+    <Logo href="/">Logo</Logo>
+    <Nav>
+      <NavItem>
+        <NavItemLink href="/explore">EXPLORE</NavItemLink>
+      </NavItem>
+      <NavItem>
+        <NavItemLink href="/portfolio">PORTFOLIO</NavItemLink>
+      </NavItem>
+      <NavItem>
+        <NavItemLink href="/login">LOGIN</NavItemLink>
+      </NavItem>
+    </Nav>
+  </HeaderStyled>
+);
+
+export default Header;
