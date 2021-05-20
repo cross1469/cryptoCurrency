ce34b6dbbb213ec6f0380201f29c869aaa325bdb
cross1469 <face9923029@gmail.com>
2021-05-20 22:16:07 +0800

    Complete Banner css style

--- src/Pages/Landing/index.js
+++ src/Pages/Landing/index.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Banner from "./Banner";
 
-const Landing = () => <>Landing</>;
+const Landing = () => (
+  <>
+    <Banner />
+  </>
+);
 
 export default Landing;
