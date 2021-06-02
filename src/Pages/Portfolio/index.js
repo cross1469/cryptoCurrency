75f1102d5c173b6bf747033d20df8a6b79f1071d
cross1469 <face9923029@gmail.com>
2021-06-02 16:42:31 +0800

    Complete wish list ui / ux

--- src/Pages/Portfolio/index.js
+++ src/Pages/Portfolio/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { color, space } from "styled-system";
 import OrderTable from "./OrderTable";
 import AssetsTotal from "./AssetsTotal";
 import WishList from "./WishList";
@@ -8,13 +7,38 @@ import YourNews from "./YourNews";
 import { subscribeUserData } from "../../Utils/firebase";
 
 const PortfolioContainer = styled.div`
-  max-width: 1200px;
-  margin: 0 auto;
+  display: flex;
+  flex: 1 1 auto;
+  align-items: center;
+  flex-direction: column;
+  padding: 0px 24px;
+  background: #181a20;
 `;
 
-const PortfolioBg = styled.div`
-  ${color}
-  ${space}
+const PortfolioContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 0 1 auto;
+  height: 100%;
+  max-height: 100%;
+  margin: 0px;
+  padding: 0px;
+  width: 100%;
+  max-width: 1280px;
+`;
+
+const PortfolioStyleFlex = styled.div`
+  display: flex;
+  flex: 1 1 auto;
+  flex-direction: column;
+  min-width: 1px;
+`;
+
+const PortfolioPanel = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  padding: 24px 0px 0px;
 `;
 
 const Portfolio = () => {
@@ -35,14 +59,20 @@ const Portfolio = () => {
   }, []);
 
   return (
-    <PortfolioBg pt={4} bg="#fafafa" pb={4}>
-      <PortfolioContainer>
-        <AssetsTotal email={email} userId={userId} />
-        <OrderTable email={email} userId={userId} />
-        <WishList email={email} userId={userId} />
-        <YourNews email={email} userId={userId} />
-      </PortfolioContainer>
-    </PortfolioBg>
+    <PortfolioContainer>
+      <PortfolioContent>
+        <PortfolioStyleFlex>
+          <PortfolioPanel>
+            <PortfolioStyleFlex>
+              <AssetsTotal email={email} userId={userId} />
+              <OrderTable email={email} userId={userId} />
+              <WishList email={email} userId={userId} />
+              <YourNews email={email} userId={userId} />
+            </PortfolioStyleFlex>
+          </PortfolioPanel>
+        </PortfolioStyleFlex>
+      </PortfolioContent>
+    </PortfolioContainer>
   );
 };
 
