a703db42d826e136106d36ce405a943abbc26e93
cross1469 <face9923029@gmail.com>
2021-06-02 23:43:42 +0800

    Complete assetsTable for desktop and mobile

--- src/Pages/Portfolio/AssetsTotal.js
+++ src/Pages/Portfolio/AssetsTotal.js
@@ -1,40 +1,142 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
-import { color, space, typography } from "styled-system";
-
+import { ReactComponent as Info } from "../../images/information.svg";
 import { firebaseReadCoinAsset, firebaseReadAsset } from "../../Utils/firebase";
 
 const AssetsContainer = styled.div`
-  display: flex;
-  box-shadow: rgb(0 0 0 / 8%) 0px 0px 4px;
-  ${space}
-  ${color}
+  margin-top: 32px;
+  color: #fff;
+  margin-bottom: 32px;
+  border-bottom: 1px solid #fff;
 `;
 
-const Account = styled.div`
+const AssetTitleContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+`;
+
+const AssetTitle = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 0 0 auto;
+  border-bottom: 1px solid #fff;
+  span {
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 32px;
+    margin: 0;
+    text-transform: uppercase;
+  }
+`;
+
+const AssetTableContainer = styled.div`
+  margin: 0px;
+  min-width: 0px;
+  display: flex;
+  border-radius: 0px;
+  padding: 8px 16px;
+`;
+
+const AssetTableContent = styled.div`
+  justify-content: space-around;
+  margin: 0px;
+  min-width: 0px;
   display: flex;
   flex-flow: column wrap;
   flex: 1 1 0%;
-  ${typography}
+  flex-direction: row;
+  @media only screen and (max-width: 576px) {
+    flex-direction: column;
+  }
 `;
-const AccountTitle = styled.div`
-  ${space}
-  ${typography}
-`;
-const AccountBalance = styled.div``;
 
-const Coin = styled.div`
+const AccountBalanceContainer = styled.div`
+  margin: 16px 32px 16px 0px;
+  min-width: 0px;
+  @media only screen and (max-width: 768px) {
+    margin-top: 8px;
+    margin-bottom: 8px;
+  }
+`;
+
+const AccountBalanceTitle = styled.div`
+  box-sizing: border-box;
+  margin: 0px;
+  min-width: 0px;
   display: flex;
-  flex-flow: column wrap;
-  flex: 1 1 0%;
-  ${typography}
+  align-items: center;
+  span {
+    margin: 0px;
+    min-width: 0px;
+    font-size: 14px;
+    line-height: 18px;
+  }
 `;
-const CoinTitle = styled.div`
-  ${space}
-  ${typography}
+
+const AccountBalanceQty = styled.div`
+  margin: 0px;
+  min-width: 0px;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-end;
+  span {
+    font-size: 32px;
+    line-height: 42px;
+    :last-child {
+      font-size: 14px;
+      line-height: 28px;
+    }
+  }
 `;
-const CoinProfitLoss = styled.div``;
+
+const AccountPNLContainer = styled.div`
+  margin: 16px 49px 16px 0px;
+  min-width: 0px;
+  display: flex;
+  flex-direction: column;
+  @media only screen and (max-width: 768px) {
+    margin-top: 8px;
+    margin-bottom: 8px;
+  }
+`;
+
+const AccountPNLTitle = styled.div`
+  margin: 0px;
+  min-width: 0px;
+  font-size: 14px;
+  line-height: 18px;
+`;
+
+const AccountPNLInfo = styled.div`
+  box-sizing: border-box;
+  margin: 0px;
+  min-width: 0px;
+  display: inline-block;
+  position: relative;
+  svg {
+    margin: 0px 0px 0px 5px;
+    position: relative;
+    top: 3px;
+    width: 16px;
+    height: 16px;
+    fill: #fff;
+  }
+`;
+
+const AccountPNLPrice = styled.div`
+  margin: 0px;
+  min-width: 0px;
+  display: flex;
+  align-items: flex-end;
+  span {
+    font-size: 32px;
+    line-height: 42px;
+  }
+  @media only screen and (max-width: 768px) {
+    align-items: flex-start;
+  }
+`;
 
 const AssetsTotal = (props) => {
   const [usdt, setUsdt] = useState({ profitLoss: null, qty: null });
@@ -61,19 +163,36 @@ const AssetsTotal = (props) => {
   }, [email]);
 
   return (
-    <AssetsContainer bg="white" mb={4} py={2} px={3}>
-      <Account fontFamily="Roboto" fontSize={16}>
-        <AccountTitle fontWeight="bold" mb={3}>
-          帳戶餘額
-        </AccountTitle>
-        <AccountBalance>{usdt.qty} USDT</AccountBalance>
-      </Account>
-      <Coin fontFamily="Roboto" fontSize={16}>
-        <CoinTitle fontWeight="bold" mb={3}>
-          貨幣盈餘
-        </CoinTitle>
-        <CoinProfitLoss>{Number(profitLoss).toFixed(2)} %</CoinProfitLoss>
-      </Coin>
+    <AssetsContainer>
+      <AssetTitleContainer>
+        <AssetTitle>
+          <span>Portfolio Balance</span>
+        </AssetTitle>
+      </AssetTitleContainer>
+      <AssetTableContainer>
+        <AssetTableContent>
+          <AccountBalanceContainer>
+            <AccountBalanceTitle>
+              <span>Account balance</span>
+            </AccountBalanceTitle>
+            <AccountBalanceQty>
+              <span>{usdt.qty}</span>
+              <span>USDT</span>
+            </AccountBalanceQty>
+          </AccountBalanceContainer>
+          <AccountPNLContainer>
+            <AccountPNLTitle>
+              Yesterday&apos;s PNL
+              <AccountPNLInfo>
+                <Info />
+              </AccountPNLInfo>
+            </AccountPNLTitle>
+            <AccountPNLPrice>
+              <span>{Number(profitLoss).toFixed(2)} %</span>
+            </AccountPNLPrice>
+          </AccountPNLContainer>
+        </AssetTableContent>
+      </AssetTableContainer>
     </AssetsContainer>
   );
 };
