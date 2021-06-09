0ca38c4a73ad7c066fa20ddff81cf5f45e56becb
cross1469 <face9923029@gmail.com>
2021-06-09 20:32:29 +0800

    fix email validation to RFC 5322

--- src/Utils/validators.js
+++ src/Utils/validators.js
@@ -2,8 +2,7 @@ const validator = {
   email: {
     rules: [
       {
-        // eslint-disable-next-line no-useless-escape
-        test: /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/,
+        test: /^(([^<>()\\[\]\\.,;:\s@"]+(\.[^<>()\\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         message: "E-mail must be a valid email",
       },
     ],
